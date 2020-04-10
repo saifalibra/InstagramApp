77f2c0e1bcc62e30d1c6e552e88636752a35cd54
shiren <raki857@mofu.be>
2020-04-10 17:57:40 +0900

    postViewControllerの追加

--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -7,11 +7,53 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class PostViewController: UIViewController {
+    var image: UIImage!
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func handlePostButton(_ sender: Any) {
+        
+        let imageData = image.jpegData(compressionQuality: 0.75)
+        let postRef = Firestore.firestore().collection(Const.Postpath).document()
+        let imageRef = Storage.storage().reference().child(Const.ImagePath).child(postRef.documentID + ".jpg")
+        
+        SVProgressHUD.show()
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        imageRef.putData(imageData!, metadata: metadata) { (metadata, error) in
+            if error != nil {
+                print(error!)
+                SVProgressHUD.showError(withStatus: "画像のアップロードが失敗しました")
+                UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
+                return
+            }
+            
+            let name = Auth.auth().currentUser?.displayName
+            let postDic = [
+                "name": name!,
+                "caption": self.textField.text!,
+                "date": FieldValue.serverTimestamp(),
+                ] as [String : Any]
+            postRef.setData(postDic)
+            SVProgressHUD.showSuccess(withStatus: "投稿しました")
+            
+            UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
+            }
+        }
+    
+    @IBAction func handleCancelButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.image = image
 
         // Do any additional setup after loading the view.
     }
